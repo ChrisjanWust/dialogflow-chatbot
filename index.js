5d1fb22dabc5090ac1d6468edfb6fa5b774b3f21
ChrisjanWust <chrisjanwust@gmail.com>
2018-11-11 01:30:28 +0200

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.post('/webhook', (req,res) => {
+    res.send(
+        {
+            "fulfillmentText": "This is a text response"
+        }
+    )
+})
+
+app.listen(3000, () => console.log('Gator app listening on port 3000!'));
